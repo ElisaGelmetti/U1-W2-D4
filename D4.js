0cc97e633630f8bd98ec64f4f900275beae79ebf
elisa gelmetti <elisagelmetti@yahoo.it>
2023-06-01 17:56:41 +0200

    correzione

--- D4.js
+++ D4.js
@@ -18,6 +18,7 @@ function area(l1, l2) {
     console.log(rettangolo)
     return rettangolo
 }
+console.log(area(4,5))
 area(5,7)
 
 
@@ -30,16 +31,15 @@ area(5,7)
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 
-function Sum() {
-    var z = 2 + 6
-    console.log("z", z)
-   return z
-   
+function crazySum(n1,n2) {
+    if( n1 === n2){
+    return (n1+n2) * 3
+}else {
+    return n1 + n2
 }
-Sum()
-Sum() *2
-
-
+ }
+console.log(crazySum(2,3))
+console.log(crazySum(3,3))
 
 
 /* ESERCIZIO 3
@@ -49,19 +49,15 @@ Sum() *2
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-function crazyDiff(x, y) {
-    x = Math.floor(x)
-    y = Math.floor(y)
-    let result = (x-y)
-    if( x <= y) {
-        result = (x - y)
+function crazyDiff(n) {
+    if( n > 19) {
+return Math.abs(n - 19) // rende il valore sempre positivo
+//  let result = n - 19 > 0 ? n - 19 : -(n - 19)
     }else {
-        result = (x - y)*3
+return Math.abs (n - 19)
+        }
     }
-    console.log.apply("differenza", result)
-    return result
-}
-
+ 
 
 
 // }
@@ -73,14 +69,19 @@ function crazyDiff(x, y) {
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 function boundary(n) {
-    if( n <= 20 && n <=100){
+    if((n >= 20 && n <=100) || n === 400){ //n è compresa da 0 a 100
         console.log("true")
+        return true
     }
     else {
         console.log("false")
+        return false
     }
 }
-boundary(77)
+console.log(boundary(30))
+console.log(boundary(400))
+console.log(boundary(401))
+
 
 
 
@@ -92,15 +93,22 @@ boundary(77)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-const x = ["EPICODE", "CORSO"]
-function epify(indexToReplace, substitution ) {
 
+function epify(str) {
+    if(
+       str.startsWith('EPICODE')
+    //    str.slice(0,7) === 'EPICODE' // 
+     
+    ){
+return str
+    } else {
+return 'EPICODE' + str
+    }
+}
 
-    x.splice(indexToReplace, 1, substitution)
+console.log(epify('EPICODE forma sviluppatore'))
 
-} 
-epify(0, "EPICODE")
-console.log("x", x)
+console.log(epify('Hello World'))
 
 // /* ESERCIZIO 6
 //  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
@@ -110,20 +118,16 @@ console.log("x", x)
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
 function check3and7 (n) {
-    if( n > 0) {
-        if(n % 3 === 0){ 
-            console.log("il numero è multiplo di 3")}
+    if( n > 0 && (n % 3 === 0 || n % 7 === 0)){ 
+            console.log("Bravo!")
+            return true
     }
-    else if(n % 7 === 0){ 
-        console.log("il numero è multiplo di 7")
-    } else {
-        console.log("il numero non è multiplo  7")
-    }
-    else {
-        console.log("il numero è negativo o ugiuale a 0")
-    }
-check3and7(7)
-
+   else {
+        return false
+   }
+}
+console.log(check3and7(14))
+console.log(check3and7(13))
 
 // operatore modulo multiplo d 3 e la divizione deve dare zero
 
@@ -134,15 +138,14 @@ check3and7(7)
 // /* SCRIVI QUI LA TUA RISPOSTA */
 // let y = ["EPICODE", "CORSO"]
 
-function reverseString(stringa) {
-    let newS =""
-    for( let i = stringa.length -1; i >= 0; i++)
-{
-newS = news + stringa.charAt(i)
-}
-console.log("reverse string", newS)
-}
+function reverseString(str) {
+    let strAsArray = str.split()
+   strAsArray.reverse()
+let reverseString = strAsArray.join('')
+return reverseString
 
+}
+console.log(reverseString('EPICODE'))
 
 // /* ESERCIZIO 8
 //  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -151,15 +154,19 @@ console.log("reverse string", newS)
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
-const upperFirst = function (string) {
-    let upperFirstString = string.toLowerCase().split('')
-for/( let i = 0; i < upperFirstString.length; i++){
-    upperFirstString[i] = upperFirstString[i].charAt(0).toUpperCase()+ upperFirstString[i].slice(1)
+function upperFirst(phrase) {
+let arrayOfWords = phrase.split(' ')
+//  ora arrayOfWords è ad es. 'EPICODE', 'forma', ' sviluppatori'
+let result = ''
+for( let i = 0; i < arrayOfWords.length; i++) {
+    result += 
+    arrayOfWords[i].charAt(0).toUpperCase() + // prende la prima lettera della parola e la mette maiuscola
+    arrayOfWords{i}.slice(1, arrayOfWords[i].length)  // e poi ci aggiungo il resto della parola
+    + ' ' // ci aggiungo uno spazio
 }
-upperFirstString = upperFirstString.join('')
-console.log(upperFirstString)
+   return result
 }
-upperFirst('Oggi è una bella giornata')
+console.log(upperFirst('Oggi è una bella giornata'))
 
 
 // /* ESERCIZIO 9
@@ -169,12 +176,12 @@ upperFirst('Oggi è una bella giornata')
 
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
-const cutString = function(string) {
-let newS = ""
-newS = string.slice(1, string.length -1)
-console.log(newS)
-
+function cutString(str) {
+let result = str.slice(1, str.length - 1) // punto iniziale, di str -1 cioè l'ultimo carattere
+return result
 }
+
+console.log(cutString('CIAO'))
 
 
 // /* ESERCIZIO 10
@@ -183,10 +190,14 @@ console.log(newS)
 // push tot volte dentro
 // /* SCRIVI QUI LA TUA RISPOSTA */
 
-let giveMeRandom = function(n){
-    let randomNumber = []
-    for ( let i = 0; i < n; i++){
-        randomNumber[i] = math.floor(math.random()*11)
+
+
+function giveMeRandom(n){
+    let result =[]
+     for ( let i = 0; i < n; i++){
+         result.push( Math.floor(Math.random()*11))
     }
+return result
 }
-giveMeRandom(9)
+
+console.log(giveMeRandom(9))
